ed8b8abc09d8b09f4dd3dfc0d5099268fa31dc8f
NaibOfDesert <tryc.artur@gmail.com>
2022-08-26 18:26:34 +0200

    Health and DamageDealer.

---
+++ CommonSpaceWarfare/Assets/Scripts/Health.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] int health = 50;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        DamageDealer damageDealer = collision.GetComponent<DamageDealer>();
+
+        if(damageDealer != null)
+        {
+            damageDealer.Hit();
+            TakeDamage(damageDealer);
+        }
+
+    }
+
+    private void TakeDamage(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- CommonSpaceWarfare/Assets/Scripts/Player.cs
+++ CommonSpaceWarfare/Assets/Scripts/Player.cs
@@ -5,10 +5,39 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    Vector2 rawInput; 
+    [SerializeField] float moveSpeed = 10f;
+
+    [SerializeField] float paddingLeft = 0f;
+    [SerializeField] float paddingRight = 0f;
+    [SerializeField] float paddingTop = 0f;
+    [SerializeField] float paddingBottom = 0f;
+    Vector2 rawInput;
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
+    void Start()
+    {
+        InitBounds();   
+    }
     void Update()
     {
-        
+        Move();
+    }
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1)); 
+    }
+    
+    
+    void Move()
+    {
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPosition = new Vector2();
+        newPosition.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPosition.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+        transform.position = newPosition;
     }
 
     void OnMove(InputValue value)
